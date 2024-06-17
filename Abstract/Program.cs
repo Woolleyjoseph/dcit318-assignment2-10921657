6906977cc616a6823d7eed045a07562d00e1255b
Woolleyjoseph <pjnwoolley001@st.ug.edu.gh>
2024-06-17 17:35:13 +0000

    Abstract Classes and Methods

---
+++ Abstract/Program.cs
@@ -0,0 +1,17 @@
+﻿using Abstract;
+using System;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myCircle = new Circle(10.0);
+            Shape myRectangle = new Rectangle(10.0, 5.0);
+
+            Console.WriteLine($"Area of Circle: {myCircle.GetArea()}");
+            Console.WriteLine($"Area of Rectangle: {myRectangle.GetArea()}");
+        }
+    }
+}
