c1c8dcb9f0f7397e9ecdb5a730be3d08bd756997
Woolleyjoseph <pjnwoolley001@st.ug.edu.gh>
2024-06-17 17:29:28 +0000

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿// Program.cs
+using Inheritance;
+using System;
+
+namespace AnimalSound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Dog myDog = new Dog();
+            Cat myCat = new Cat();
+
+            myAnimal.MakeSound();
+            myDog.MakeSound();
+            myCat.MakeSound();
+        }
+    }
+}
